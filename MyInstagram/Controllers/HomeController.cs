c103cca9e893e039ecf45f927ae2faaba8b4a5e8
Callum Barker <callum.scott.barker@gmail.com>
2018-06-11 11:34:43 +0800

    login and register moved to index page

--- MyInstagram/Controllers/HomeController.cs
+++ MyInstagram/Controllers/HomeController.cs
@@ -3,16 +3,39 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using MyInstagram.Models;
+using MyInstagram.Models.AccountViewModels;
 
 namespace MyInstagram.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ILogger<HomeController> logger
+        )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            LoginRegisterViewModel LRVM = new LoginRegisterViewModel();
+            LRVM.Login = GetLoginViewModel();
+            LRVM.Register = GetRegisterViewModel();
+
+            return View(LRVM);
         }
 
         public IActionResult About()
@@ -32,6 +55,65 @@ namespace MyInstagram.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private RegisterViewModel GetRegisterViewModel()
+        {
+            return new RegisterViewModel();
+        }
+
+        private LoginViewModel GetLoginViewModel()
+        {
+            return new LoginViewModel();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(LoginRegisterViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Register.Email, Email = model.Register.Email };
+                var result = await _userManager.CreateAsync(user, model.Register.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
+                    //await _emailSender.SendEmailConfirmationAsync(model.Register.Email, callbackUrl);
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    _logger.LogInformation("User created a new account with password.");
+                    return RedirectToLocal(returnUrl);
+                }
+                AddErrors(result);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View("Index", model);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
         }
     }
 }
---
+++ MyInstagram/Models/LoginRegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyInstagram.Models.AccountViewModels
+{
+    public class LoginRegisterViewModel
+    {
+        public LoginViewModel Login { get; set; }
+        public RegisterViewModel Register { get; set; }
+    }
+}
